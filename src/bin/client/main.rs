f4795291792bcef112a19a69839ced6aed953334
Josef Simonson <josefatsimonson@gmail.com>
2023-03-08 13:08:04 +0100

    Create a section only for debug builds

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -17,40 +17,45 @@ enum AppState {
 }
 
 fn main() {
-    App::new()
-        // .insert_resource(ScheduleRunnerSettings::run_loop(Duration::from_millis(120)))
-        .register_type::<Threat>()
-        .add_plugins(
-            DefaultPlugins
-                .set(WindowPlugin {
-                    window: WindowDescriptor {
-                        title: String::from("Tiled Map Editor Example"),
-                        ..Default::default()
-                    },
-                    ..default()
-                })
-                .set(ImagePlugin::default_nearest())
-                .set(AssetPlugin {
-                    watch_for_changes: true,
-                    asset_folder: String::from(""),
-                    ..default()
-                }),
-        )
-        .add_plugin(WorldInspectorPlugin)
-        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
-        .add_plugin(RapierDebugRenderPlugin::default())
-        .add_plugin(game::GamePlugin)
-        .add_plugin(NetworkPlugin)
-        .add_state(AppState::Connecting)
-        .add_system_set(
-            SystemSet::new()
-                .with_run_criteria(run_if_client_connected)
-                .with_system(set_running_state),
-        )
-        .add_startup_system(setup_graphics)
-        .add_system(helpers::camera::movement)
-        .add_system(bevy::window::close_on_esc)
-        .run();
+    let mut app = App::new();
+    // .insert_resource(ScheduleRunnerSettings::run_loop(Duration::from_millis(120)))
+    app.add_plugins(
+        DefaultPlugins
+            .set(WindowPlugin {
+                window: WindowDescriptor {
+                    title: String::from("Tiled Map Editor Example"),
+                    ..Default::default()
+                },
+                ..default()
+            })
+            .set(ImagePlugin::default_nearest())
+            .set(AssetPlugin {
+                watch_for_changes: true,
+                asset_folder: String::from(""),
+                ..default()
+            }),
+    )
+    .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+    .add_plugin(game::GamePlugin)
+    .add_plugin(NetworkPlugin)
+    .add_state(AppState::Connecting)
+    .add_system_set(
+        SystemSet::new()
+            .with_run_criteria(run_if_client_connected)
+            .with_system(set_running_state),
+    )
+    .add_startup_system(setup_graphics)
+    .add_system(helpers::camera::movement)
+    .add_system(bevy::window::close_on_esc);
+
+    #[cfg(debug_assertions)]
+    {
+        app.register_type::<Threat>()
+            .add_plugin(WorldInspectorPlugin)
+            .add_plugin(RapierDebugRenderPlugin::default());
+    }
+
+    app.run();
 }
 
 fn set_running_state(mut state: ResMut<State<AppState>>, client: Res<RenetClient>) {
